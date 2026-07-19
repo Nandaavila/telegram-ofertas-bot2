c689486e5b0d1fae0943add714be248f88c5615e
Fernanda <fernandaoavila19@gmail.com>
2026-07-18 21:56:44 -0300

    fix: corrige aspas no horario de teste

--- config.py
+++ config.py
@@ -149,7 +149,7 @@ COLUNAS_RELATORIO_ML = {
 # AGENDAMENTO
 # ---------------------------------------------------------------------
 # Horários em que o robô vai buscar e publicar ofertas (formato 24h).
-HORARIOS_PUBLICACAO = ["06:30", "09:00", "12:30", "16:00", "19:30", "21:00", "21:30", "21:55"]
+HORARIOS_PUBLICACAO = ["06:30", "09:00", "12:30", "16:00", "19:30", "21:00", "21:30", "22:03"]
 
 # Intervalo (em minutos) entre a busca de novas ofertas nos marketplaces
 INTERVALO_BUSCA_MINUTOS = 60
